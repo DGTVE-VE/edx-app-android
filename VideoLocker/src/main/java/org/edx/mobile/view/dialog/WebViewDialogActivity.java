7f70c2eb3a0058c46cd834c73a2ac4c560dbb5c0
mdinino <mdinino@gmail.com>
2016-09-01 13:59:34 -0400

    Making Forgot your password and TOS accessible

--- VideoLocker/src/main/java/org/edx/mobile/view/dialog/WebViewDialogActivity.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/dialog/WebViewDialogActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import org.edx.mobile.R;
 import org.edx.mobile.base.BaseAppActivity;
@@ -66,17 +66,13 @@ public class WebViewDialogActivity extends BaseAppActivity {
 
         webView.loadUrl(getIntent().getStringExtra(ARG_URL));
 
-        final TextView tv_dialog_title = (TextView) findViewById(R.id.tv_dialog_title);
-        final View viewSeperator = findViewById(R.id.view_seperator);
         final String title = getIntent().getStringExtra(ARG_TITLE);
-        if (TextUtils.isEmpty(title)) {
-
-            tv_dialog_title.setVisibility(View.INVISIBLE);
-            viewSeperator.setVisibility(View.INVISIBLE);
-        } else {
-            tv_dialog_title.setVisibility(View.VISIBLE);
-            viewSeperator.setVisibility(View.VISIBLE);
-            tv_dialog_title.setText(title);
+        if (!TextUtils.isEmpty(title)) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            setTitle(title);
+        }
+        else {
+            getSupportActionBar().hide();
         }
 
         findViewById(R.id.positiveButton).setOnClickListener(new OnClickListener() {
@@ -86,6 +82,17 @@ public class WebViewDialogActivity extends BaseAppActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     public void onResume() {
